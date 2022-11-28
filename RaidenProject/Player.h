fb226cc28107a367e4fe3b3a926e7d553120d075
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2022-11-28 14:37:08 +0900

    プレイヤー処理にKey入力追加終了

--- RaidenProject/Player.h
+++ RaidenProject/Player.h
@@ -14,15 +14,15 @@ private:
 	int Score; //スコア
 	int Life; //残り残機
 	int Player_images[4]; //プレイヤーイメージ画像
-	int imagesnum; //画像番号
-	float Player_X;
-	float Player_Y;
+	int imagesnum; //プレイヤー画像番号
+	float Player_X; //プレイヤーX座標
+	float Player_Y; //プレイヤーY座標
 
 
 public:
-	Player();
-	virtual void Update()override;
-	virtual void Draw()const override; 
+	Player(); //コンストラクタ
+	virtual void Update()override; //描画処理以外
+	virtual void Draw()const override; //描画
 	virtual void Hit()override;
 	void LifeCheck();
 	int GetScore();
--- RaidenProject/player.cpp
+++ RaidenProject/player.cpp
@@ -9,7 +9,9 @@ Player::Player()
 	Player_Y = 0;
 	imagesnum = 0;
 	LoadDivGraph("images/Player/Zerofighter plane.png", 4, 32, 1, 32, 32, Player_images);
-
+	g_KeyFlg = 0;
+	g_NowKey = 0;
+	g_OldKey = 0;
 }
 
 void Player::Update() 
@@ -19,10 +21,10 @@ void Player::Update()
 	g_NowKey = GetJoypadInputState(DX_INPUT_PAD1);
 	g_KeyFlg = g_NowKey & ~g_OldKey;
 
-	if (GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_LEFT) --Player_X; //ç∂
-	if (GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_RIGHT) ++Player_X; //âE
-	if (GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_UP) --Player_Y; //è„
-	if (GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_DOWN) ++Player_Y; //â∫
+	if (g_KeyFlg & PAD_INPUT_LEFT) --Player_X; //ç∂
+	if (g_KeyFlg & PAD_INPUT_RIGHT) ++Player_X; //âE
+	if (g_KeyFlg & PAD_INPUT_UP) --Player_Y; //è„
+	if (g_KeyFlg & PAD_INPUT_DOWN) ++Player_Y; //â∫
 }
 
 void Player::Draw() const
